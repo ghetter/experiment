36a4a1ea669caa6d54e9bd287e8afd9463512588
ghetter <49289242+ghetter@users.noreply.github.com>
2021-09-29 08:56:50 +0600

    some fixes in jquery code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,62 @@
 $(document).ready(function(){
 
-// Навигационное меню <начало>
-    // Иконка для мобильных устройств
-    $('.icon-menu').click(function(event){
-        $('body').toggleClass('locked');
-        $(this).toggleClass('active');
-        $('.primary-navigation__body').toggleClass('active');
-    });
-    
-    $('.primary-menu__item.menu__item-has-child').hover(function(event){
-        $('.secondary-menu').addClass('showed')
-    }, function(){
-        $('.secondary-menu').removeClass('showed')
-    });
+// Готовые, и нормально работающие скрипты
 
-    $('.secondary-menu__item.menu__item-has-child').hover(function(event){
-        $('.tertiary-menu').addClass('showed')
-    }, function(){
-        $('.tertiary-menu').removeClass('showed')
-    }, $('.secondary-menu__item.menu__item-has-child').click(function(event){
-        $('.tertiary-menu').toggleClass('showed')
-    }));
+    // Навигационное меню. В зависимости от версии сайта, мобильная это версия или нет, будут срабатывать скрипты.
+    // Для мобильных устройств не доступны события .hover, лишь события .click. На компьютерной версии, доступно и то, и другое.
 
-// Навигационное меню </конец>
-// Поиск <начало>
-    $('.search__button').hover(function(event){
-        $(this).toggleClass('focused')
-    }, function(){
-        $(this).removeClass('focused')
-    });
 
-// Поиск </конец>
+
+
+    if($(window).width() < 1023)
+    {
+        $('.icon-menu').click(function(event){
+            $(this).toggleClass('active');
+            $('.primary-navigation__body').toggleClass('active');
+            $('.menu__item-has-child').removeClass('toggled');
+            $('.secondary-menu').removeClass('showed');
+            $('.tertiary-menu').removeClass('showed');
+        });
+
+        $('.primary-menu__item.menu__item-has-child >div').click(function(event){
+            $('.primary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.secondary-menu').toggleClass('showed');
+        });
+
+        $('.secondary-menu__item.menu__item-has-child >div').click(function(event){
+            $('.secondary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.tertiary-menu').toggleClass('showed');
+        });
+    } else {
+        $('.icon-menu').click(function(event){
+            $(this).toggleClass('active');
+            $('.primary-navigation__body').toggleClass('active');
+        });
+
+        $('.primary-menu__item.menu__item-has-child >div').click(function(event){
+            $('.primary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.secondary-menu').toggleClass('showed');
+        });
+
+        $('.secondary-menu__item.menu__item-has-child >div').click(function(event){
+            $('.secondary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.tertiary-menu').toggleClass('showed');
+        });
+
+        $('.primary-menu__item.menu__item-has-child').hover(function(event_pc){
+            $('.primary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.secondary-menu').toggleClass('showed');
+        });
+
+        $('.secondary-menu__item.menu__item-has-child').hover(function(event_pc){
+            $('.secondary-menu__item.menu__item-has-child').toggleClass('toggled');
+            $('.tertiary-menu').toggleClass('showed');
+        });
+    }
+
+
+    // Исчезновение кнопки "назад" в Pagintaion, когда мы находимся на первой странице
+
     $('.page-first').each(function(){
         if ($(this).hasClass('page-active')){
             $('.pagination__button-back').toggleClass('hidden')
@@ -39,6 +65,8 @@ $(document).ready(function(){
         }
     });
 
+    // Исчезновение кнопки "вперёд" в Pagintaion, когда мы находимся на последней странице
+
     $('.page-last').each(function(){
         if ($(this).hasClass('page-active')){
             $('.pagination__button-next').toggleClass('hidden')
@@ -46,6 +74,8 @@ $(document).ready(function(){
             $('.pagination__button-next').removeClass('hidden')
         }
     });
+
+    // При нажатии на кнопку "+1" или +более скрытые теги начинают отображаться.
 
     $('.item-wrapped').click(function(){
         $(this).addClass('unwrapped')
